0fc3a9acce8c9938fc207263b2e0a5dbc4b815e9
Duc Anh Le <gerald.duc.anh.le@gmail.com>
2025-05-22 00:47:06 +0300

    implementing the frontend with all the services

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,41 @@
-import buildClient from "../api/build-client";
+import Link from "next/link";
 
-const LandingPage = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You're signed in.</h1>
-  ) : (
-    <h1>You're not logged in.</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => (
+    <tr key={ticket.id}>
+      <td>{ticket.title}</td>
+      <td>{ticket.price}</td>
+      <td>
+        <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+          View
+        </Link>
+      </td>
+    </tr>
+  ));
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
-LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get("/api/users/currentuser");
-  return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  // const client = buildClient(context);
+  // const { data } = await client.get("/api/users/currentuser");
+  // return data;
+  const { data } = await client.get("/api/tickets");
+  return { tickets: data };
 };
 
 export default LandingPage;
