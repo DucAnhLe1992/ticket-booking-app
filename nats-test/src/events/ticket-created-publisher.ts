24d8cbcee08d2193f1f4b631547430e31b2f0d87
Duc Anh Le <gerald.duc.anh.le@gmail.com>
2025-05-22 23:49:58 +0300

    Delete nats-test directory

--- nats-test/src/events/ticket-created-publisher.ts
+++
@@ -1,8 +0,0 @@
-import { Publisher } from "./base-publisher";
-import { TicketCreatedEvents } from "./ticket-created-events";
-import { Subjects } from "./subjects";
-import { TicketCreatedListener } from "./ticket-created-listener";
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvents> {
-  readonly subject = Subjects.TicketCreated;
-}
