280d38cf67ab0ed82aab5d3af9e3c81e28787159
Duc Anh Le <gerald.duc.anh.le@gmail.com>
2025-06-10 11:47:16 +0300

    Update build-client.js

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,7 @@ const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     // server side
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://www.ticket-system.xyz/",
       headers: req.headers,
     });
   } else {
